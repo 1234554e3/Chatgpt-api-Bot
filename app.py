990060ec47824e67af6ad54964fc82140cddca2e
Rohit Bedse <123181434+1234554e3@users.noreply.github.com>
2023-08-15 19:30:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import openai
+
+# Set your OpenAI API key here
+openai.api_key = "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"
+
+# Welcome message
+st.title("Intelligent Chatbot")
+st.write("Welcome to the Intelligent Chatbot! Type your message below.")
+
+# User input
+user_input = st.text_input("You:", "")
+
+if user_input:
+    # Generate a response using OpenAI
+    response = openai.Completion.create(
+        engine="text-davinci-003",  # You can experiment with different engines
+        prompt=user_input,
+        max_tokens=50  # You can adjust the response length
+    )
+    
+    # Display the bot's response
+    bot_response = response.choices[0].text.strip()
+    st.text("ChatBot:")
+    st.write(bot_response)
